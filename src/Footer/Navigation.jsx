02e882d04813a8f1a4d3a582741e66c83470be54
rsredsq <rsredsq@gmail.com>
2018-01-05 17:08:27 +0300

    added navigation component

---
+++ src/Footer/Navigation.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Col } from 'react-flexbox-grid';
+
+const Container = ({ children }) => (
+  <Col mdOffset={1} lgOffset={1} md={2} lg={2}>
+    {children}
+  </Col>
+);
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Link = styled.a`
+  text-decoration: none;
+  color: #383838;
+  font-weight: 300;
+  margin-bottom: 0.5rem;
+`;
+
+const Title = Link.extend`
+  font-weight: bold;
+  margin-bottom: 1rem;
+`;
+
+const Airbnb = () => (
+  <Container>
+    <Nav>
+      <Title href="#">Airbnb</Title>
+      <Link href="#">About us</Link>
+      <Link href="#">Careers</Link>
+      <Link href="#">Press</Link>
+      <Link href="#">Policies</Link>
+      <Link href="#">Help</Link>
+      <Link href="#">Diversity & Belonging</Link>
+    </Nav>
+  </Container>
+);
+
+const Discover = () => (
+  <Container>
+    <Nav>
+      <Title href="#">Discover</Title>
+      <Link href="#">Trust & Safety</Link>
+      <Link href="#">Travel Credit</Link>
+      <Link href="#">Gift Cards</Link>
+      <Link href="#">Airbnb Citizen</Link>
+      <Link href="#">Business Travel</Link>
+      <Link href="#">Guidebooks</Link>
+      <Link href="#">Airbnbmag</Link>
+    </Nav>
+  </Container>
+);
+
+const Hosting = () => (
+  <Container>
+    <Nav>
+      <Title href="#">Hosting</Title>
+      <Link href="#">Why Host</Link>
+      <Link href="#">Hospitality</Link>
+      <Link href="#">Responsible Hosting</Link>
+      <Link href="#">Community Center</Link>
+    </Nav>
+  </Container>
+);
+
+export default () => (
+  <React.Fragment>
+    <Airbnb />
+    <Discover />
+    <Hosting />
+  </React.Fragment>
+);
--- src/Footer/index.jsx
+++ src/Footer/index.jsx
@@ -3,11 +3,13 @@ import styled from 'styled-components';
 
 import Selectors from './Selectors';
 import { Grid, Row } from 'react-flexbox-grid';
+import Navigation from './Navigation';
 
 export default () => (
   <Grid>
     <Row>
       <Selectors />
+      <Navigation />
     </Row>
   </Grid>
 );
