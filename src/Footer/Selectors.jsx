9b67c71af3481d4a95b8065bea3df14a4963fe0d
rsredsq <rsredsq@gmail.com>
2018-01-06 14:24:21 +0300

    Remove unnecessary div in selectors

--- src/Footer/Selectors.jsx
+++ src/Footer/Selectors.jsx
@@ -5,7 +5,6 @@ import { Col } from 'react-flexbox-grid';
 import arrowDown from '../assets/arrow-down.svg';
 
 const Wrapper = styled.div`
-  width: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -16,46 +15,34 @@ const Wrapper = styled.div`
   }
 `;
 
-const Dropdown = styled.div`
-  width: 100%;
-  flex: 1 1 100%;
-  border: 1px solid rgba(72, 72, 72, 0.2);
-  overflow: hidden;
-  box-sizing: border-box;
-  box-shadow: 0 2px 4px rgba(72, 72, 72, 0.08);
-  margin-bottom: 1rem;
-  border-radius: 4px;
-  margin-right: 1rem;
-`;
-
 const Select = styled.select`
   width: 100%;
   height: 3rem;
   padding-left: 1rem;
   padding-right: 1rem;
-  border: none;
+  margin-bottom: 1rem;
+  margin-right: 1rem;
   outline: none;
-  box-sizing: padding-box;
   appearance: none;
+
   background: url(${arrowDown}) right center no-repeat;
   background-origin: content-box;
+  border: 1px solid rgba(72, 72, 72, 0.2);
+  box-shadow: 0 2px 4px rgba(72, 72, 72, 0.08);
+  border-radius: 4px;
   color: #383838;
 `;
 
 const LanguageSelector = () => (
-  <Dropdown>
-    <Select>
-      <option>English</option>
-    </Select>
-  </Dropdown>
+  <Select>
+    <option>English</option>
+  </Select>
 );
 
 const CurrencySelector = () => (
-  <Dropdown>
-    <Select>
-      <option>United States dollar</option>
-    </Select>
-  </Dropdown>
+  <Select>
+    <option>United States dollar</option>
+  </Select>
 );
 
 export default () => (
