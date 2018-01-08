6b6e4ce787b840dc44b79e10c90fac18f4b243a6
rsredsq <rsredsq@gmail.com>
2018-01-08 23:37:26 +0300

    fix sticky headers z-index

--- src/Filters/index.jsx
+++ src/Filters/index.jsx
@@ -8,6 +8,7 @@ const Wrapper = styled.div`
   left: 0;
   right: 0;
   background: white;
+  z-index: 1000;
   border-bottom: 1px solid rgba(72, 72, 72, 0.3);
 `;
 
--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -13,6 +13,7 @@ const Header = styled.header`
   left: 0;
   right: 0;
   background: white;
+  z-index: 1000;
   border-bottom: 1px solid rgba(72, 72, 72, 0.3);
 `;
 
