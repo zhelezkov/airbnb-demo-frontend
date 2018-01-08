ba60db2b30e878595926064c1231eec065a77328
rsredsq <rsredsq@gmail.com>
2018-01-08 18:47:16 +0300

    added more mock data

--- src/Homes/mockData.js
+++ src/Homes/mockData.js
@@ -1,6 +1,9 @@
 import salentinaImage from './images/salentina.png';
 import private3BedrImage from './images/private3Bedr.png';
-import dreamHouseImage from '../Homes/images/dreamHouse.png';
+import dreamHouseImage from './images/dreamHouse.png';
+import luxuryLoftImage from './images/luxuryLoft.png';
+import lussuosoImage from './images/lussuoso.png';
+import lecceImage from './images/lecce.png';
 
 export default [
   {
@@ -33,6 +36,39 @@ export default [
     houseType: 'Entire treehouse',
     beds: 1,
     reviews: 364,
+    rating: 5,
+    hostType: 'Superhost'
+  },
+  {
+    id: 3,
+    title: 'Best location old town luxury loft',
+    image: luxuryLoftImage,
+    price: 110,
+    houseType: 'Entire apartment',
+    beds: 1,
+    reviews: 369,
+    rating: 5,
+    hostType: 'Superhost'
+  },
+  {
+    id: 4,
+    title: 'Lussuoso. Vista incantevole.',
+    image: lussuosoImage,
+    price: 83,
+    houseType: 'Entire apartment',
+    beds: 6,
+    reviews: 105,
+    rating: 5,
+    hostType: 'Superhost'
+  },
+  {
+    id: 5,
+    title: 'In the historical center of Lecce',
+    image: lecceImage,
+    price: 72,
+    houseType: 'Entire house',
+    beds: 3,
+    reviews: 221,
     rating: 5,
     hostType: 'Superhost'
   }
