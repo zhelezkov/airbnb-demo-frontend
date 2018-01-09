2eefc2435216c6d53a73a35b6906d24638755536
rsredsq <rsredsq@gmail.com>
2018-01-09 23:05:10 +0300

    more content padding

--- src/Homes/index.jsx
+++ src/Homes/index.jsx
@@ -4,7 +4,7 @@ import { Route } from 'react-router-dom';
 import List from './List';
 
 const Content = styled.div`
-  padding-top: 3.5625rem;
+  padding-top: 5.0625rem;
 `;
 
 export default () => (
