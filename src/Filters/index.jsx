8ea578bd5076d254fabdeedf07761eaab393d858
rsredsq <rsredsq@gmail.com>
2018-01-12 23:07:03 +0300

    filters border style fix

--- src/Filters/index.jsx
+++ src/Filters/index.jsx
@@ -11,7 +11,7 @@ const Wrapper = styled.div`
   right: 0;
   background: white;
   z-index: 1000;
-  border-bottom: 1px solid rgba(72, 72, 72, 0.3);
+  border-bottom: 0.5px solid rgba(72, 72, 72, 0.3);
 `;
 
 const Row = styled(BasicRow)`
