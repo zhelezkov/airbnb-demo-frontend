a18f7b0e96cfa741e057b3e16b4e2435e99a4a91
rsredsq <rsredsq@gmail.com>
2018-01-13 22:34:01 +0300

    fix date picker button title

--- src/Filters/DatePicker/helpers.js
+++ src/Filters/DatePicker/helpers.js
@@ -2,7 +2,7 @@ import moment from 'moment';
 
 export function buttonDateFormat(startDate, endDate) {
   const format = (date) => date.format('MMM DD');
-  return `${format(startDate)}–${format(endDate)}`;
+  return `${format(startDate)} – ${format(endDate)}`;
 }
 
 export const daysBeforeToday = day => {
