8002e27f2ed025b69ef285bd85189609fa16ff9f
rsredsq <rsredsq@gmail.com>
2018-01-13 17:01:49 +0300

    date delimiter fix

--- src/Filters/DatePicker/helpers.js
+++ src/Filters/DatePicker/helpers.js
@@ -1,4 +1,4 @@
 export function buttonDateFormat(startDate, endDate) {
   const format = (date) => date.format('MMM DD');
-  return `${format(startDate)}-${format(endDate)}`;
+  return `${format(startDate)}–${format(endDate)}`;
 }
