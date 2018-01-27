601a9ef383f677ffe37b65782f28d769a4c5e7b3
rsredsq <rsredsq@gmail.com>
2018-01-27 21:30:50 +0300

    better price button title

--- src/Filters/Price/helpers.js
+++ src/Filters/Price/helpers.js
@@ -1,6 +1,6 @@
 export function getButtonTitle(min, max) {
   if (min !== 10 || max !== 1000) {
-    return `From ${min} to ${max}`;
+    return `From $${min} to $${max}`;
   }
   return 'Price';
 }
