a504a5d37ab7224c2055953592157b2c074d13fa
Nikita Zhelezkov <rsredsq@gmail.com>
2018-02-01 22:46:08 +0300

    switch-case replaced with map extracted api data normalization function

--- src/Homes/api.js
+++ src/Homes/api.js
@@ -1,30 +1,24 @@
-function mapHouseType(houseType) {
-  switch (houseType) {
-    case 'entire_home':
-      return 'Entire house';
-    case 'private_room':
-      return 'Private room';
-    default:
-      return '';
-  }
-}
+const houseTypes = {
+  entire_home: 'Entire house',
+  private_room: 'Private room',
+};
+
+const normalizeData = (home) => ({
+  id: home.id,
+  title: home.name,
+  image: home.images[0].picture,
+  price: home.price,
+  houseType: houseTypes[home.kind],
+  beds: home.bedsCount,
+  reviews: home.reviewsCount,
+  hostType: home.isSuperhost ? 'Superhost' : '',
+  rating: Math.ceil(home.rating),
+  lat: home.lat,
+  lng: home.lng,
+});
 
 export function retrieveHomesData(offset = 0, limit = 6) {
   return fetch(`https://airbnb-demo-api.now.sh/v1/homes?offset=${offset}&limit=${limit}`)
     .then(res => res.json())
-    .then(res => res.items)
-    .then(res =>
-      res.map(home => ({
-        id: home.id,
-        title: home.name,
-        image: home.images[0].picture,
-        price: home.price,
-        houseType: mapHouseType(home.kind),
-        beds: home.bedsCount,
-        reviews: home.reviewsCount,
-        hostType: home.isSuperhost ? 'Superhost' : '',
-        rating: Math.ceil(home.rating),
-        lat: home.lat,
-        lng: home.lng,
-      })));
+    .then(res => res.items.map(normalizeData));
 }
